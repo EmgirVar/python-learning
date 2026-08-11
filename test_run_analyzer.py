e8bc34c32b641a3d67973d90f8d3ab772b99e74d
EmgirVar <mrfill37@gmail.com>
2026-08-11 14:32:15 +0300

    feat: add test run analyzer

---
+++ test_run_analyzer.py
@@ -0,0 +1,51 @@
+id_1 = 1
+name_1 = "Авторизация пользователя"
+priority_1 = "High"
+status_1 = "Passed"
+
+id_2 = 2
+name_2 = "Прикрепление почты"
+priority_2 = "Medium"
+status_2 = "Failed"
+
+id_3 = 3
+name_3 = "Смена пароля"
+priority_3 = "Low"
+status_3 = "Blocked"
+
+id_4 = 4
+name_4 = "Удаление УЗ"
+priority_4 = "Medium"
+status_4 = "Not Run"
+
+test_cases = [
+    [id_1, name_1, priority_1, status_1 ],
+    [id_2, name_2, priority_2, status_2 ],
+    [id_3, name_3, priority_3, status_3 ],
+    [id_4, name_4, priority_4, status_4 ]
+]
+
+all_case = 0
+case_passed = 0
+case_failed = 0
+case_blocked = 0
+case_not_run = 0
+
+for test in test_cases:
+    print(f"ТС#{test[0]}: {test[1]} | Приоритет: {test[2]} | Статус: {test[3]}")
+    all_case += 1
+    if test[3] == "Passed": 
+        case_passed += 1
+    elif test[3] == "Failed":
+        case_failed += 1
+    elif test[3] == "Blocked":
+        case_blocked += 1
+    elif test[3] == "Not Run":
+        case_not_run += 1
+
+print("=== ИТОГИ ПРОГОНА ===")
+print(f"Всего тестов: {all_case}")
+print(f"Passed: {case_passed}")
+print(f"Failed: {case_failed}")
+print(f"Blocked: {case_blocked}")
+print(f"Not Run: {case_not_run}")
